a2c400c8da5f204e1450b7a0bfb022c424560d28
Mahbub Rahman <lover.mahabub@gmail.com>
2017-12-26 17:52:16 +0600

    Login form with client side validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom';
+
+import "semantic-ui-css/semantic.min.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
